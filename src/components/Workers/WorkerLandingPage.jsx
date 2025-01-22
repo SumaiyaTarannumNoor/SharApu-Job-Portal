bfe3f45ea83f63d14b5d849776871558188f2993
SumaiyaTarannumNoor <sumaiyatarannumnoor001@gmail.com>
2025-01-22 12:06:30 +0600

    Worker Landin Page Component Updated

--- src/components/Workers/WorkerLandingPage.jsx
+++ src/components/Workers/WorkerLandingPage.jsx
@@ -32,43 +32,43 @@ const WorkerLandingPage = () => {
   const workers = [
     {
       imageSrc: Worker1,
-      title: "ダブルワーク＋ご家庭のバランスの取り方とは",
-      description: "現在は総務所にて医療事務の仕事とfondesk のオペレーター業務を..."
+      title: "How to balance double work + family life",
+      description: "Currently working as a medical office worker and a fondesk operator at the general office..."
     },
     {
       imageSrc: Worker2,
-      title: "転勤や育児との両立は在宅オペレータの仕事で全てクリア",
-      description: "最初は国内の損害保険会社で3年ほど事務職に就いていました。夫の..."
+      title: "Work as a home-based operator makes it all possible to balance work and childcare.",
+      description: "At first, I worked in a clerical position at a domestic non-life insurance company for about three years. Husband's..."
     },
     {
       imageSrc: Worker3,
-      title: "ライティングを中心に月5万円の収入",
-      description: "時間に拘束されず、趣味の園芸の時間もあって、とても時間に余裕..."
+      title: "Income of 50,000 yen per month mainly from writing",
+      description: "I am not restricted by time and have time for my hobby of gardening, so I have a lot of free time..."
     },
     {
       imageSrc: Worker4,
-      title: "ママと子供が笑顔になれば社会が変わる！",
-      description: "まじめなワーカーさんが多いので現在は安心して発注できるシュ..."
+      title: "When mothers and children smile, society changes!",
+      description: "There are many serious workers, so now you can order with confidence..."
     },
     {
       imageSrc: Worker5,
-      title: "事業のコアを強化するためクラウドソーシングを活用",
-      description: "初めて依頼した方から非常にクオリティの高い文章を書いていた..."
+      title: "Leveraging crowdsourcing to strengthen the core of your business",
+      description: "The person who requested it for the first time wrote an extremely high quality text..."
     },
     {
       imageSrc: Worker6,
-      title: "クラウドソーシングは業務拡張の要！",
-      description: "主婦とか女性のワーカーさんはトラブルが少なく良い記事を書い..."
+      title: "Crowdsourcing is the key to business expansion!",
+      description: "Housewives and female workers have fewer troubles and write better articles..."
     },
     {
       imageSrc: Worker7,
-      title: "ライティングを強みに持つシュフティで効率アップ",
-      description: "主に依頼している仕事内容はブログ記事のライティングで、大量の..."
+      title: "Increase efficiency with Shufuti, whose strength is writing.",
+      description: "The main job I am asked to do is write blog articles, which involves a large amount of..."
     },
     {
       imageSrc: Worker8,
-      title: "何気なく始めた在宅ワークは最高の恩恵き！",
-      description: "日中は子供たちがいるので仕事をすることはできません。ですの..."
+      title: "Working from home, which I started casually, has been a great blessing!",
+      description: "I can't work during the day because I have children. It's..."
     },
   ];
 
@@ -76,10 +76,10 @@ const WorkerLandingPage = () => {
     <section className="container mx-auto px-4 py-12">
       <div className="mt-16">
         <h2 className="text-xl sm:text-2xl font-bold text-pink-500 mb-4">
-          主婦の在宅ワーカーさんが多数活躍中！
+        Many housewives working from home are active!
         </h2>
         <p className="text-base sm:text-lg mb-8">
-          時間の使い方や仕事の選び方など、参考になるインタビューを多数掲載
+        Contains many useful interviews on how to use time and how to choose a job
         </p>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 justify-items-center">
           {workers.map((worker, index) => (
@@ -88,7 +88,7 @@ const WorkerLandingPage = () => {
         </div>
         <div className="text-center mt-12">
           <button className="bg-white text-pink-500 border-2 border-pink-500 px-8 py-3 rounded-full text-xl hover:bg-pink-100 transition duration-300">
-            インタビューをもっと見る
+            View More Interviews
           </button>
         </div>
       </div>
