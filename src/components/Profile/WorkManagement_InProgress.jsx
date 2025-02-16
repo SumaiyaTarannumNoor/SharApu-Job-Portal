5919876397a7611c41b4fa6aa6b63632bd6a24e1
SumaiyaTarannumNoor <sumaiyatarannumnoor001@gmail.com>
2025-02-16 13:30:49 +0600

    WorkManagement on HirerProfile Updated

---
+++ src/components/Profile/WorkManagement_InProgress.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WorkManagement_InProgress = () => {
+  return (
+    <div className="bg-white p-8 rounded-lg shadow-sm border border-gray-200">
+      <p className="text-gray-600">There is currently no work in progress.</p>
+    </div>
+  );
+};
+
+export default WorkManagement_InProgress;
